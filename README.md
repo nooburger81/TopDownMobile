4986ba37171ed96b2029858e1730e166531a6fd2
nooburger81 <brandonsebastian@gmail.com>
2024-02-28 23:10:43 -0500

    content so far

---
+++ README.md
@@ -0,0 +1,3 @@
+# TopDownMobile
+
+Developed with Unreal Engine 5
---
+++ Source/TopDownMobile/TopDownMobileGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TopDownMobileGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOPDOWNMOBILE_API ATopDownMobileGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
